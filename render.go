a50f95705ff61455d5ba51e5ed4623bf2b527f70
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-13 19:08:35 +1000

    add about button

--- render.go
+++ render.go
@@ -3,12 +3,21 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 func render_home(w fyne.Window) fyne.CanvasObject {
+	about_btn := widget.NewToolbarAction(theme.QuestionIcon(), func() {
+		txt := widget.NewRichTextFromMarkdown(`This program is licensed under the MIT license`)
+
+		d := dialog.NewCustom("About aixoio's privacy tools", "Ok", txt, w)
+		d.Show()
+	})
+
 	return container.NewBorder(
-		widget.NewLabel("aixoio's privacy tools"),
+		widget.NewToolbar(widget.NewToolbarSpacer(), about_btn),
 		nil,
 		nil,
 		nil,
