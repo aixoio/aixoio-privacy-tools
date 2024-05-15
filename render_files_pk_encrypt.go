5b346ce1c71efcb09728ee791820f12d483ec8b7
David L Terpin <aixoio@yahoo.com>
2024-05-15 13:44:18 +1000

    add empty pk files encrypt page

---
+++ render_files_pk_encrypt.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+)
+
+func render_files_pk_encrypt(w fyne.Window) fyne.CanvasObject {
+
+	backbtn := widget.NewButtonWithIcon("Back to menu", theme.NavigateBackIcon(), func() { w.SetContent(render_files(w)) })
+	path := ""
+	path_wid := widget.NewLabel(path)
+	path_key := ""
+	path_wid_key := widget.NewLabel(path_key)
+	opts := []string{"PGP", "RSA"}
+	sel_wid := widget.NewSelect(opts, func(s string) {})
+	sel_wid.SetSelectedIndex(0)
+
+	actbtn := widget.NewButton("Encrypt", func() {
+
+	})
+	actbtn.Disable()
+
+	return container.NewBorder(
+		container.NewGridWithColumns(
+			3,
+			backbtn,
+			widget.NewLabel("Files - Public key - Encrypt"),
+			widget.NewLabel(""),
+		),
+		actbtn,
+		nil,
+		nil,
+		container.NewPadded(
+			container.New(
+				layout.NewFormLayout(),
+				widget.NewLabel("File"),
+				container.NewGridWithColumns(
+					2,
+					path_wid,
+					widget.NewButton("Select file", func() {
+						dialog.ShowFileOpen(func(uc fyne.URIReadCloser, err error) {
+							if uc == nil {
+								return
+							}
+							if err != nil {
+								show_err(w)
+								return
+							}
+
+							path = uc.URI().Path()
+							path_wid.SetText(uc.URI().Name())
+							if path_key != "" && path != "" {
+								actbtn.Enable()
+							} else {
+								actbtn.Disable()
+							}
+						}, w)
+					}),
+				),
+				widget.NewLabel("Public key"),
+				container.NewGridWithColumns(
+					2,
+					path_wid_key,
+					widget.NewButton("Select file", func() {
+						dialog.ShowFileOpen(func(uc fyne.URIReadCloser, err error) {
+							if uc == nil {
+								return
+							}
+							if err != nil {
+								show_err(w)
+								return
+							}
+
+							path_key = uc.URI().Path()
+							path_wid_key.SetText(uc.URI().Name())
+							if path_key != "" && path != "" {
+								actbtn.Enable()
+							} else {
+								actbtn.Disable()
+							}
+						}, w)
+					}),
+				),
+				widget.NewLabel("Cipher"),
+				sel_wid,
+			),
+		),
+	)
+}
