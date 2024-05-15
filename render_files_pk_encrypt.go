ce378b97dd414e5d532df57f8df7fa6a78567fa2
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-15 17:41:40 +1000

    add function to pk encrypt page

--- render_files_pk_encrypt.go
+++ render_files_pk_encrypt.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"os"
+	"sync"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"github.com/ProtonMail/gopenpgp/v2/helper"
+	"github.com/aixoio/aixoio-privacy-tools/lib/rsahelper"
 )
 
 func render_files_pk_encrypt(w fyne.Window) fyne.CanvasObject {
@@ -21,6 +26,108 @@ func render_files_pk_encrypt(w fyne.Window) fyne.CanvasObject {
 	sel_wid.SetSelectedIndex(0)
 
 	actbtn := widget.NewButton("Encrypt", func() {
+		file_dat, err := os.ReadFile(path)
+		if err != nil {
+			show_err(w)
+			return
+		}
+
+		pk_file_dat, err := os.ReadFile(path_key)
+		if err != nil {
+			show_err(w)
+			return
+		}
+
+		switch sel_wid.SelectedIndex() {
+		case 0: // PGP
+			var wg sync.WaitGroup
+
+			wg.Add(1)
+
+			var out string
+
+			go func() {
+				defer wg.Done()
+				out, err = helper.EncryptBinaryMessageArmored(string(pk_file_dat), file_dat)
+			}()
+
+			d := dialog.NewCustomWithoutButtons("Encrypting - "+path_wid.Text, container.NewPadded(
+				widget.NewProgressBarInfinite(),
+			), w)
+
+			d.Show()
+
+			wg.Wait()
+
+			d.Hide()
+
+			if err != nil {
+				show_err(w)
+				return
+			}
+
+			dialog.ShowFileSave(func(uc fyne.URIWriteCloser, err error) {
+				if uc == nil {
+					return
+				}
+				if err != nil {
+					show_err(w)
+					return
+				}
+
+				_, err = uc.Write([]byte(out))
+				if err != nil {
+					show_err(w)
+					return
+				}
+
+				dialog.ShowInformation("File saved", "The file was saved", w)
+
+			}, w)
+		case 1: // RSA
+			var wg sync.WaitGroup
+
+			wg.Add(1)
+
+			var out []byte
+
+			pk_key := rsahelper.ExportPEMStrToPubKey(pk_file_dat)
+
+			go func() {
+				defer wg.Done()
+				out = rsahelper.Rsa_enc(pk_key, file_dat)
+			}()
+
+			d := dialog.NewCustomWithoutButtons("Encrypting - "+path_wid.Text, container.NewPadded(
+				widget.NewProgressBarInfinite(),
+			), w)
+
+			d.Show()
+
+			wg.Wait()
+
+			d.Hide()
+
+			dialog.ShowFileSave(func(uc fyne.URIWriteCloser, err error) {
+				if uc == nil {
+					return
+				}
+				if err != nil {
+					show_err(w)
+					return
+				}
+
+				_, err = uc.Write(out)
+				if err != nil {
+					show_err(w)
+					return
+				}
+
+				dialog.ShowInformation("File saved", "The file was saved", w)
+
+			}, w)
+
+		}
 
 	})
 	actbtn.Disable()
