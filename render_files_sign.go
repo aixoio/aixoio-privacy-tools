b282bd8d7adffc33e803943506b6ae87b5af12ad
David L Terpin <79969530+aixoio@users.noreply.github.com>
2024-05-15 19:04:42 +1000

    add function to pgp sign

--- render_files_sign.go
+++ render_files_sign.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"os"
+	"strings"
+	"sync"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"github.com/ProtonMail/gopenpgp/v2/crypto"
 )
 
 func render_files_sign(w fyne.Window) fyne.CanvasObject {
@@ -20,7 +25,73 @@ func render_files_sign(w fyne.Window) fyne.CanvasObject {
 	sel_wid.SetSelectedIndex(0)
 
 	actbtn := widget.NewButton("Sign", func() {
+		file_dat, err := os.ReadFile(path)
+		if err != nil {
+			show_err(w)
+			return
+		}
 
+		pk_key, err := os.ReadFile(path_key)
+		if err != nil {
+			show_err(w)
+			return
+		}
+
+		switch sel_wid.SelectedIndex() {
+		case 0: // PGP
+			var wg sync.WaitGroup
+
+			wg.Add(1)
+
+			var out string
+
+			go func() {
+				defer wg.Done()
+				var ring *crypto.Key
+				ring, err = crypto.NewKeyFromArmoredReader(strings.NewReader(string(pk_key)))
+				var key *crypto.Key
+				key, err = ring.Unlock([]byte("aixoio-privacy-tools"))
+				var key_ring_sign *crypto.KeyRing
+				key_ring_sign, err = crypto.NewKeyRing(key)
+				var sig *crypto.PGPSignature
+				sig, err = key_ring_sign.SignDetached(crypto.NewPlainMessage(file_dat))
+				out, err = sig.GetArmored()
+			}()
+
+			d := dialog.NewCustomWithoutButtons("Signing - "+path_wid.Text, container.NewPadded(
+				widget.NewProgressBarInfinite(),
+			), w)
+
+			d.Show()
+
+			wg.Wait()
+
+			d.Hide()
+
+			if err != nil {
+				show_err(w)
+				return
+			}
+
+			dialog.ShowFileSave(func(uc fyne.URIWriteCloser, err error) {
+				if uc == nil {
+					return
+				}
+				if err != nil {
+					show_err(w)
+					return
+				}
+
+				_, err = uc.Write([]byte(out))
+				if err != nil {
+					show_err(w)
+					return
+				}
+
+				dialog.ShowInformation("File saved", "The file was saved", w)
+
+			}, w)
+		}
 	})
 	actbtn.Disable()
 
